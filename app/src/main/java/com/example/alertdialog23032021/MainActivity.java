40231ed6ab28d6a179c822bac586d8ec47dc18e1
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-15 21:12:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/alertdialog23032021/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.alertdialog23032021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Person;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
