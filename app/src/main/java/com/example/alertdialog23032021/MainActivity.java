b81123d9928ff2b9e053200ecf8ca28c9e34730e
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-17 19:58:15 +0700

    sự kiện cho các nút

--- app/src/main/java/com/example/alertdialog23032021/MainActivity.java
+++ app/src/main/java/com/example/alertdialog23032021/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Person;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle("Bạn có thông báo mới");
                 builder.setMessage("Có phiên bản update mới");
                 builder.setIcon(R.mipmap.ic_launcher);
+                builder.setCancelable(false);
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Bạn chọn có", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Bạn chọn không", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Bạn chọn hủy", Toast.LENGTH_SHORT).show();
+                    }
+                });
                 builder.show();
             }
         });
